01e553aaf1d3558b28bc7f40ee591a2ac13b4cc0
Aniket7876 <aniketgore700@gmail.com>
2025-07-19 13:31:39 +0530

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 
 // Connect to the laptop’s WebSocket server (replace with your ngrok URL)
-const ws = new WebSocket('https://shaky-women-kick.loca.lt');
+const ws = new WebSocket('https://tidy-humans-shake.loca.lt');
 
 ws.on('open', () => {
   console.log('Connected to local laptop’s browser');
