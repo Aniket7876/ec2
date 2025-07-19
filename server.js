c9ca0f946c73fb166ab7b2b107a48e6512184fd2
Aniket7876 <aniketgore700@gmail.com>
2025-07-19 13:26:20 +0530

    test

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const WebSocket = require('ws');
 
 // Connect to the laptop’s WebSocket server (replace with your ngrok URL)
-const ws = new WebSocket('https://many-cows-jump.loca.lt');
+const ws = new WebSocket('https://fair-planes-hammer.loca.lt');
 
 ws.on('open', () => {
   console.log('Connected to local laptop’s browser');
 
   // Send a scraping command
   ws.send(JSON.stringify({
-    url: 'http://books.toscrape.com/',
-    action: 'scrape',
-    selector: 'title', // Example: Scrape the page title
+    tracking_number: "SSESEA2504249893",
+    type: 'mbl',
+    
   }));
 });
 
 ws.on('message', (data) => {
   const response = JSON.parse(data);
   if (response.status === 'success') {
-    console.log('Scraped data:', response.result);
+    const fs = require('fs');
+    fs.writeFileSync('test.json', JSON.stringify(response.result, null, 2));
+    console.log('Scraped data saved to test.json');
+    
   } else {
     console.error('Error:', response.message);
   }
