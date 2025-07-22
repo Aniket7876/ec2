8a78c3e65f06256b2cbb688c54fd6a7484db30f5
Aniket7876 <aniketgore700@gmail.com>
2025-07-22 14:44:03 +0530

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 
 // Connect to the laptop’s WebSocket server (replace with your ngrok URL)
-const ws = new WebSocket('https://early-games-remain.loca.lt');
+const ws = new WebSocket('https://bright-ghosts-cheer.loca.lt');
 
 ws.on('open', () => {
   console.log('Connected to local laptop’s browser');
