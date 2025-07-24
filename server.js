a699f61c03b1fb8743291b16f3801814c4b7c4cc
Aniket7876 <aniketgore700@gmail.com>
2025-07-24 18:58:02 +0530

    t

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ region: 'ap-south-1' });
 
 // Connect to the laptop’s WebSocket server
-const ws = new WebSocket('https://thirty-baboons-lick.loca.lt');
+const ws = new WebSocket('https://five-llamas-refuse.loca.lt');
 
 ws.on('open', () => {
   console.log('Connected to local laptop’s browser');
