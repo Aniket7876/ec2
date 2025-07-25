e5abc2c9798573386cb4a05ef8e677ccf586d80d
Aniket7876 <aniketgore700@gmail.com>
2025-07-25 11:29:56 +0530

    comment

--- server.js
+++ server.js
@@ -67,32 +67,32 @@
 //   }));
 // });
 
-ws.on('message', (data) => {
-  const response = JSON.parse(data);
+// ws.on('message', (data) => {
+//   const response = JSON.parse(data);
 
-  if (response.status === 'success') {
-    const s3Key = `${response.code.toLowerCase()}/${response.trackingNumber.toLowerCase()}.json`;
-    const params = {
-      Bucket: 'testbucketaniket7876',
-      Key: s3Key,
-      Body: JSON.stringify(response.rawData, null, 2),
-      ContentType: 'application/json'
-    };
+//   if (response.status === 'success') {
+//     const s3Key = `${response.code.toLowerCase()}/${response.trackingNumber.toLowerCase()}.json`;
+//     const params = {
+//       Bucket: 'testbucketaniket7876',
+//       Key: s3Key,
+//       Body: JSON.stringify(response.rawData, null, 2),
+//       ContentType: 'application/json'
+//     };
 
-    s3.putObject(params, (err) => {
-      if (err) {
-        console.error('S3 upload error:', err);
-      } else {
-        console.log(`Scraped data saved to s3://testbucketaniket7876/${s3Key}`);
-      }
-    });
-  } else {
-    console.error('Error:', response.message);
-  }
-});
+//     s3.putObject(params, (err) => {
+//       if (err) {
+//         console.error('S3 upload error:', err);
+//       } else {
+//         console.log(`Scraped data saved to s3://testbucketaniket7876/${s3Key}`);
+//       }
+//     });
+//   } else {
+//     console.error('Error:', response.message);
+//   }
+// });
 
-ws.on('error', (error) => console.error('WebSocket error:', error));
-ws.on('close', () => console.log('Disconnected from laptop'));
+// ws.on('error', (error) => console.error('WebSocket error:', error));
+// ws.on('close', () => console.log('Disconnected from laptop'));
 
 const WebSocket = require('ws');
 const AWS = require('aws-sdk');
