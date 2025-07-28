1127bb2b1b7da024e2797088f35321a4fb233837
Aniket7876 <aniketgore700@gmail.com>
2025-07-28 22:25:04 +0530

    test number

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const WebSocket = require('ws');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ region: 'ap-south-1' });
 
-const ws = new WebSocket('wss://hamilton-highest-totally-doing.trycloudflare.com');
+const ws = new WebSocket('wss://mention-lawn-untitled-guy.trycloudflare.com');
 
 const scrapingTasks = [
   { tracking_number: "SSESEA2504249893", type: 'mbl', code: 'UWLD' },
