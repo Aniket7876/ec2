9fa05b2f671756718c87ef91ac6a226aa3badcb7
Aniket7876 <aniketgore700@gmail.com>
2025-08-19 15:44:02 +0530

    new

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const WebSocket = require('ws');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ region: 'ap-south-1' });
 
-const ws = new WebSocket('wss://photography-via-tiger-settled.trycloudflare.com');
+const ws = new WebSocket('wss://known-ride-trailer-wp.trycloudflare.com');
 
 const scrapingTasks = [
   { tracking_number: "SSESEA2504249893", type: 'mbl', code: 'UWLD' },
