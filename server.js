e151bbc1a7893dae2731039acedbabc0adabce1b
Aniket7876 <aniketgore700@gmail.com>
2025-08-28 17:10:27 +0530

    test zimu

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const WebSocket = require('ws');
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const { type } = require('os');
 const s3 = new AWS.S3({ region: 'ap-south-1' });
 
 // Connect to the laptop’s WebSocket server
-const ws = new WebSocket('wss://performances-ecommerce-valid-afghanistan.trycloudflare.com');
+const ws = new WebSocket('wss://societies-facing-homeless-cameras.trycloudflare.com');
 
 ws.on('open', () => {
   console.log('Connected to local laptop’s browser');
 
   const scrapingTasks = [
-  { tracking_number: "SSESEA2504249893", type: 'mbl', code: 'UWLD' },
+  // { tracking_number: "SSESEA2504249893", type: 'mbl', code: 'UWLD' },
   { tracking_number: "SSECLE2403203859", type: 'mbl', code: 'UWLD' },
-  { tracking_number: "254851590", type: 'mbl', code: 'MAEU' },
-  { tracking_number: "MRKU8203610", type: 'mbl', code: 'MAEU' },
-  { tracking_number: "SNLFNJJL001257", type: 'mbl', code: '12IH' },
+  // { tracking_number: "254851590", type: 'mbl', code: 'MAEU' },
+  // { tracking_number: "MRKU8203610", type: 'mbl', code: 'MAEU' },
+  // { tracking_number: "SNLFNJJL001257", type: 'mbl', code: '12IH' },
   { tracking_number: "253450396", type: 'mbl', code: 'MAEU' },
-  { tracking_number: "254866453", type: 'mbl', code: 'MAEU' },
+  // { tracking_number: "254866453", type: 'mbl', code: 'MAEU' },
   { tracking_number: "254527448", type: 'mbl', code: 'MAEU' },
-  { tracking_number: "254198838", type: 'mbl', code: 'MAEU' },
+  // { tracking_number: "254198838", type: 'mbl', code: 'MAEU' },
   {tracking_number: "GDY0384003", type: "mbl", code: "CMDU"},
-  {tracking_number: "GDY0385735", type: "mbl", code: "CMDU"},
-  {tracking_number: "ANT1901431", type: "mbl", code: "CMDU"},
+  // {tracking_number: "GDY0385735", type: "mbl", code: "CMDU"},
+  // {tracking_number: "ANT1901431", type: "mbl", code: "CMDU"},
+  // {tracking_number: "GOSUIAU3037988", type: "mbl", code: "ZIMU"},
+  {tracking_number: "ZIMUSNH22125519", type: "mbl", code: "ZIMU"}
   ];
 
   ws.send(JSON.stringify({
@@ -48,6 +52,13 @@ ws.on('message', (data) => {
         console.log(`Scraped data saved to s3://testbucketaniket7876/${s3Key}`);
       }
     });
+    // fs.writeFile('data.json', JSON.stringify(response, null, 2), (err) => {
+    //   if (err) {
+    //     console.error('File write error:', err);
+    //   } else {
+    //     console.log('Scraped data saved to data.json');
+    //   }
+    // });
   } else {
     console.error('Error:', response.message);
   }
