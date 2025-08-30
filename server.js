2bdaee6f9b91bc11b6b2cd5a85835b580c62345a
Aniket7876 <aniketgore700@gmail.com>
2025-08-30 19:10:54 +0530

    test

--- server.js
+++ server.js
@@ -5,29 +5,29 @@ const { type } = require('os');
 const s3 = new AWS.S3({ region: 'ap-south-1' });
 
 // Connect to the laptop’s WebSocket server
-const ws = new WebSocket('wss://classical-jamie-author-dollar.trycloudflare.com');
+const ws = new WebSocket('wss://bracelet-yet-bumper-pn.trycloudflare.com');
 
 ws.on('open', () => {
   console.log('Connected to local laptop’s browser');
 
   const scrapingTasks = [
-    // { tracking_number: "SSESEA2504249893", type: 'mbl', code: 'UWLD' },
-    // { tracking_number: "SSECLE2403203859", type: 'mbl', code: 'UWLD' },
+    { tracking_number: "SSESEA2504249893", type: 'mbl', code: 'UWLD' },
+    { tracking_number: "SSECLE2403203859", type: 'mbl', code: 'UWLD' },
     { tracking_number: "254851590", type: 'mbl', code: 'MAEU' },
     { tracking_number: "MRKU8203610", type: 'mbl', code: 'MAEU' },
-    // { tracking_number: "SNLFNJJL001257", type: 'mbl', code: '12IH' },
-    // { tracking_number: "SNLFSHJLE8A0361", type: 'mbl', code: "12IH"},
-    // { tracking_number: "SNLFSHJLE8A0386", type: 'mbl', code: "12IH"},
+    { tracking_number: "SNLFNJJL001257", type: 'mbl', code: '12IH' },
+    { tracking_number: "SNLFSHJLE8A0361", type: 'mbl', code: "12IH" },
+    { tracking_number: "SNLFSHJLE8A0386", type: 'mbl', code: "12IH" },
     { tracking_number: "253450396", type: 'mbl', code: 'MAEU' },
     { tracking_number: "254866453", type: 'mbl', code: 'MAEU' }, 
     { tracking_number: "254527448", type: 'mbl', code: 'MAEU' },
     { tracking_number: "254198838", type: 'mbl', code: 'MAEU' },
-    // {tracking_number: "GDY0384003", type: "mbl", code: "CMDU"},
-    // {tracking_number: "GDY0385735", type: "mbl", code: "CMDU"},
-    // {tracking_number: "ANT1901431", type: "mbl", code: "CMDU"},
-    // {tracking_number: "ZIMUSIN8154785", type: "mbl", code: "ZIMU"},
-    // {tracking_number: "ZIMUSNH22125519", type: "mbl", code: "ZIMU"},
-    // {tracking_number: "ZIMUSNH22204594", type: "mbl", code: "ZIMU"},
+    { tracking_number: "GDY0384003", type: "mbl", code: "CMDU" },
+    { tracking_number: "GDY0385735", type: "mbl", code: "CMDU" },
+    { tracking_number: "ANT1901431", type: "mbl", code: "CMDU" },
+    { tracking_number: "ZIMUSIN8154785", type: "mbl", code: "ZIMU" },
+    { tracking_number: "ZIMUSNH22125519", type: "mbl", code: "ZIMU" },
+    { tracking_number: "ZIMUSNH22204594", type: "mbl", code: "ZIMU" },
   ];
 
   ws.send(JSON.stringify({
